730cb1c642dbc0aac1a5706e0341804e88c4f49a
josuewsilva <josuewsilva@gmail.com>
2022-09-09 18:24:31 -0300

    adicionado o arqivo da data

---
+++ JS/date.js
@@ -0,0 +1,13 @@
+
+
+var extenso = new Date();
+var dayWeek = extenso.getDay();
+var day = extenso.getDate();
+var month = extenso.getMonth();
+var year = extenso.getFullYear();
+
+var week = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
+var months = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+
+var dateComplete = document.getElementById("#date");
+dateComplete.innerHTML=`Boa tarde! Hoje é ${week[dayWeek]}, ${day} de ${months[month]} de ${year}!`;
