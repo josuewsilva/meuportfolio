2e4a1a822ce68571c1258d22935317a00982337b
josuewsilva <josuewsilva@gmail.com>
2022-09-16 19:10:48 -0300

    mais uma seção na página principal e início de rodapé

--- JS/date.js
+++ JS/date.js
@@ -9,5 +9,5 @@ var year = extenso.getFullYear();
 var week = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
 var months = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
 
-var dateComplete = document.getElementById("date");
+//var dateComplete = document.getElementById("date");
 dateComplete.innerHTML=`Boa tarde! Hoje é ${week[dayWeek]}, ${day} de ${months[month]} de ${year}!`;
